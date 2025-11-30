2c4b44b890a7599992fb7c0662f91fdc680fccb2
Steven Dao <stevendao100@gmail.com>
2025-11-30 17:08:30 -0500

    change job parameters

--- hybrid/submit.sh
+++ hybrid/submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FILES=$(for i in {1..40}; do echo ../omp/text_inputs/random_6400000.txt; done)
 
-sbatch --ntasks-per-node=1 --cpus-per-task=8 -o output.txt ./sbatch.sh ./bin/wc $FILES
-sbatch --ntasks-per-node=16 --cpus-per-task=8 -o output.txt ./sbatch.sh ./bin/wc $FILES
+sbatch --ntasks-per-node=1 --cpus-per-task=4 -o output_p1.txt ./sbatch.sh ./bin/wc $FILES
+sbatch --ntasks-per-node=16 --cpus-per-task=4 -o output_p16.txt ./sbatch.sh ./bin/wc $FILES
