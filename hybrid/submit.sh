861f926a51dbda329faa63297fe0c166bc7b0bce
Steven Dao <stevendao100@gmail.com>
2025-11-30 17:06:24 -0500

    different tests

--- hybrid/submit.sh
+++ hybrid/submit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#
-sbatch --ntasks-per-node=2 --cpus-per-task=8 -o output.txt ./sbatch.sh ./bin/wc ../omp/text_inputs/random_6400000.txt
+FILES=$(for i in {1..40}; do echo ../omp/text_inputs/random_6400000.txt; done)
+
+sbatch --ntasks-per-node=1 --cpus-per-task=8 -o output.txt ./sbatch.sh ./bin/wc $FILES
+sbatch --ntasks-per-node=16 --cpus-per-task=8 -o output.txt ./sbatch.sh ./bin/wc $FILES
